c6c2f4d735288de1fa77eac8a0870d038291d7f3
clive <clive@1ui-MacBook-Pro.local>
2016-06-14 16:03:27 +0900

    collect basic info

--- collector/models.py
+++ collector/models.py
@@ -5,3 +5,6 @@ class billInfo(models.Model):
     billNum = models.CharField(max_length=10, default=None, unique=True)
     billName = models.CharField(max_length=300, default=None)
     billUrl = models.URLField(max_length=100, default=None, null=True, blank=True)
+
+class billAcceptInfo(models.Model):
+    bill = models.ForeignKey(billInfo)
--- collector/views.py
+++ collector/views.py
@@ -6,37 +6,42 @@ import bs4
 import requests
 
 def list_collector(request):
-    r = requests.Session()
-    r.headers.update({'referer': 'http://likms.assembly.go.kr/bill/main.do'})
-    front_url = 'http://likms.assembly.go.kr/bill/billDetail.do?billId='
-
-    # for page in range(1, 191):
-    #     data = r.get(
-    #         'http://likms.assembly.go.kr/bill/BillSearchResult.do?ageFrom=19&ageTo=19&pageSize=100&strPage='+str(page)
-    #     )
-    #     data = data.text.encode(data.encoding)
-    #     data = bs4.BeautifulSoup(data)
-
-    #     table = data.find("div", attrs={'class': 'tableCol01'})
-    #     trs = table.findAll('tr')
-
-    #     for tr in trs[1:]:
-    #         tds = tr.findAll('td')
-    #         link = tds[1].find('a').get('href')
-    #         name = tds[1].text
-    #         num = tds[0].text
-    #         link = front_url + link.replace('javascript:fGoDetail(\'', '').replace('\', \'\')', '')
-    #         try:
-    #             billInfo.objects.create(billNum=num, billName=name, billUrl=link)
-    #         except:
-    #             pass
-    #     print("####### page" + str(page) + " complete #######")
-    #     print("current number of objects: " + str(billInfo.objects.count()))
 
     bills = billInfo.objects.all()
     context = {'bills': bills}
 
     return TemplateResponse(request, 'index.html', context)
+
+# get basic information for each bills.
+def bill_url_collector(agefrom, ageto, pagesize, maxpage):
+    r = requests.Session()
+    r.headers.update({'referer': 'http://likms.assembly.go.kr/bill/main.do'})
+    front_url = 'http://likms.assembly.go.kr/bill/billDetail.do?billId='
+
+    for page in range(1, maxpage + 1): # to end of the pages
+        data = r.get(
+            'http://likms.assembly.go.kr/bill/BillSearchResult.do?ageFrom=' + str(agefrom)
+            + '&ageTo=' + str(ageto) + '&pageSize=' + str(pagesize) + '&strPage=' + str(page)
+        ) # ageForm -> 회기시작, ageTo -> 회기종료, pageSize -> 페이지당 의안 수, strPage -> 확인할 페이지 
+        data = data.text.encode(data.encoding)
+        data = bs4.BeautifulSoup(data)
+
+        table = data.find("div", attrs={'class': 'tableCol01'})
+        trs = table.findAll('tr')
+
+        for tr in trs[1:]:
+            tds = tr.findAll('td')
+            link = tds[1].find('a').get('href')
+            name = tds[1].text
+            num = tds[0].text
+            link = front_url + link.replace('javascript:fGoDetail(\'', '').replace('\', \'\')', '')
+            try:
+                billInfo.objects.create(billNum=num, billName=name, billUrl=link)
+            except:
+                pass
+
+        print("####### page" + str(page) + " complete #######")
+        print("current number of objects: " + str(billInfo.objects.count()))
 
 def bill_detail(info):
     r = requests.Session()
@@ -45,4 +50,26 @@ def bill_detail(info):
     data = r.get(info.billUrl)
     data = data.text.encode(data.encoding)
     data = bs4.BeautifulSoup(data)
+
+    process = data.find("span", attrs={"class": "on"}).text
+
+    cotents = data.findAll("div", attrs={"class": "contIn"})
+
+    # contIn div 아래 subti h5가 분류 결정
+    # contIn div 안에 테이블에서 데이터 추출
+    billnum = ""
+    for cont in contents:
+        type = cont.find("h5", attrs={"class": "subti01"}).text
+
+        if type == "의안접수정보":
+            tbody = cont.find("tbody")
+            tr = cont.find("tr")
+            tds = tr.find("td")
+            billnum = td[0].text
+            billProposeDate = td[1].text
+            billProposer = td[2].text
+
+
+
+
 
